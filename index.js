b607ca1b92db5505b9df666f3d70b09042a69d24
Tal Safran <talsafran@github.com>
2020-12-23 13:58:25 +0200

    omg it works -- major scale!

--- index.js
+++ index.js
@@ -1,47 +1,87 @@
 NUMBER_OF_CHARTS = 1
 
 config = {
-  frets: 14
+  frets: 14,
+  position: 1
 }
 
-const legacyChordOptions = {
-  fingers: [
-    [1, 0],
-    [2, 1],
-    [3, 2],
-    [4, 2],
-    [5, 0],
-  ],
+const defaultChordOptions = {
+  fingers: [],
   barres: [],
-  position: 0,
+  position: 1,
 }
 
-const stringIntervals = {
-  6: 5,
-  5: 5,
-  4: 5,
-  3: 4,
-  2: 5,
+const stringIntervals = [
+  [6, 0],
+  [5, 5],
+  [4, 5],
+  [3, 5],
+  [2, 4],
+  [1, 5]
+]
+
+const majorScaleIntervals = {
+  1: 2,
+  2: 2,
+  3: 1,
+  4: 2,
+  5: 2,
+  6: 2,
+  7: 1,
 }
 
-const startingString = 5
-
-const startingFret = 2
-const endingFret = 14
-
-function chordOptions() {
-  const defaultOptions = { barres: [] }
-}
+const startingString = 6
+const startingPosition = 3
 
 function drawChart() {
   const charts = document.querySelectorAll('.chart')
+
   for (let chartElement of charts) {
     let chart = new svguitar.SVGuitarChord(chartElement)
 
-    chart.chord(legacyChordOptions)
+    let fingerOptions = generateFingerings()
+    let chordOptions = {...defaultChordOptions, ...fingerOptions}
+
+    // debugger
+
+    chart.chord(chordOptions)
     chart.configure(config)
     chart.draw()
   }
+}
+
+function generateFingerings() {
+  let fingerings = []
+  let currentPosition = startingPosition
+  let currentScaleDegree = 1
+
+  for (const [string, interval] of stringIntervals) {
+    const stringNumber = parseInt(string)
+
+    currentPosition = currentPosition - interval - 12
+
+    while (currentPosition <= config.frets) {
+      if (currentPosition >= config.position) {
+        fingerings.push([stringNumber, currentPosition, currentScaleDegree.toString()])
+      }
+
+      // debugger
+
+      const nextPosition = currentPosition + majorScaleIntervals[currentScaleDegree]
+      if (nextPosition > config.frets) break
+
+      currentPosition = nextPosition
+
+      if (currentScaleDegree === 7) {
+        currentScaleDegree = 1
+      } else {
+        currentScaleDegree++
+      }
+    }
+    // console.log('string, interval', stringNumber, interval)
+  }
+
+  return { fingers: fingerings }
 }
 
 (function() {
