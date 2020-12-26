b5dfe499e4a2865f8d0b85e8132dc8fe06627264
Tal Safran <talsafran@github.com>
2020-12-26 18:34:49 +0200

    constants like to YELL

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const COLORS = {
   others: '#ccc',
 }
 
-const config = {
+const CONFIG = {
   frets: 12,
   position: 1,
   title: 'Gmaj7',
   titleFontSize: 36,
 }
 
-const defaultChordOptions = {
+const DEFAULT_CHORD_OPTIONS = {
   fingers: [],
   barres: [],
   position: 1,
 }
 
-const stringIntervals = [
+const STRING_INTERVALS = [
   [6, 0],
   [5, 5],
   [4, 5],
@@ -28,7 +28,7 @@ const stringIntervals = [
   [1, 5],
 ]
 
-const majorScaleIntervals = {
+const MAJOR_SCALE_INTERVALS = {
   1: 2,
   2: 2,
   3: 1,
@@ -38,10 +38,10 @@ const majorScaleIntervals = {
   7: 1,
 }
 
-const degreesToDraw = [1, 3, 5, 7]
+const DEGREES_TO_DRAW = [1, 3, 5, 7]
 
-const startingString = 6
-const startingPosition = 3
+const STARTING_STRING = 6
+const STARTING_POSITION = 3
 
 function drawChart() {
   const charts = document.querySelectorAll(CHART_ELEMENT_SELECTOR)
@@ -50,27 +50,27 @@ function drawChart() {
     let chart = new svguitar.SVGuitarChord(chartElement)
 
     let fingerOptions = generateFingerings()
-    let chordOptions = {...defaultChordOptions, ...fingerOptions}
+    let chordOptions = {...DEFAULT_CHORD_OPTIONS, ...fingerOptions}
 
     chart.chord(chordOptions)
-    chart.configure(config)
+    chart.configure(CONFIG)
     chart.draw()
   }
 }
 
 function generateFingerings() {
   let fingerings = []
-  let currentPosition = startingPosition
+  let currentPosition = STARTING_POSITION
   let currentScaleDegree = 1
 
-  for (const [string, interval] of stringIntervals) {
+  for (const [string, interval] of STRING_INTERVALS) {
     const stringNumber = parseInt(string)
 
     currentPosition = currentPosition - interval - 12
 
-    while (currentPosition <= config.frets) {
-      const currentPositionIsVisible = currentPosition >= config.position
-      const shouldDraw = currentPositionIsVisible && degreesToDraw.includes(currentScaleDegree)
+    while (currentPosition <= CONFIG.frets) {
+      const currentPositionIsVisible = currentPosition >= CONFIG.position
+      const shouldDraw = currentPositionIsVisible && DEGREES_TO_DRAW.includes(currentScaleDegree)
 
       if (shouldDraw) {
         const styling = stylingForScaleDegree(currentScaleDegree)
@@ -78,8 +78,8 @@ function generateFingerings() {
         fingerings.push([stringNumber, currentPosition, styling])
       }
 
-      const nextPosition = currentPosition + majorScaleIntervals[currentScaleDegree]
-      if (nextPosition > config.frets) break
+      const nextPosition = currentPosition + MAJOR_SCALE_INTERVALS[currentScaleDegree]
+      if (nextPosition > CONFIG.frets) break
 
       currentPosition = nextPosition
       currentScaleDegree = nextScaleDegree(currentScaleDegree)
